8db97beb5c249c081fd31c9aa35cde06b405ef5e
Vanessa <vgr140502@gmail.com>
2025-08-11 13:10:17 -0400

    first commit

---
+++ header.js
@@ -0,0 +1,28 @@
+function actualizarContenido() {
+  const inicio = document.getElementById("inicio");
+  const servicio = document.getElementById("servicios");
+  const quienesSomos = document.getElementById("quienesSomo");
+  const contacto = document.getElementById("contacto");
+  const ancho = window.innerWidth;
+
+  if (ancho <= 428) {
+    inicio.innerHTML = `<i class="fa-solid fa-house fa-lg" style="color: #ffffff"></i
+          >`;
+    servicio.innerHTML = `<span class="material-symbols-outlined">
+home_repair_service
+</span>`;
+    quienesSomos.innerHTML = `<i class="fa-solid fa-building fa-lg" style="color: #ffffff;"></i>`;
+    contacto.innerHTML = `<i class="fa-solid fa-phone fa-lg" style="color: #ffffff;"></i>`;
+  } else {
+    inicio.textContent = "Inicio";
+    servicio.textContent = "Servicio";
+    quienesSomos.textContent = "¿Quiénes somos?";
+    contacto.textContent = "Contáctanos";
+  }
+}
+
+// Ejecuta al cargar la página
+window.addEventListener("load", actualizarContenido);
+
+// Ejecuta cada vez que se cambia el tamaño de la ventana
+window.addEventListener("resize", actualizarContenido);
